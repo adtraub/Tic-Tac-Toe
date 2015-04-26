41e656da52ec9855e8f3276ff833bfec294a8210
adtraub <adamtraubsemail@gmail.com>
2015-04-26 00:03:54 -0400

    initial commit

---
+++ AI.py
@@ -0,0 +1,250 @@
+"""
+Adam Traub
+Tic-Tac-Toe AI
+4/25/2015
+"""
+import random
+
+
+class AI:
+    """Unbeatable AI for Tic Tac Toe
+
+        Args:
+          board (Board): The board currently in play
+          character (str): The character the AI places on the board
+          opponentChar (str): The opponent's TTT character
+          spotsTaken (int): keeps track of the number of moves made.
+
+        Attributes:
+          board (Board): The board currently in play
+          character (str): The character the AI places on the board
+          opponentChar (str): The opponent's TTT character
+          corners (dict): contains a reference to all the corners
+          sides (dict): contains a reference to all the sides
+          randomSides (list): values from sides in random order
+          randomCorners (list): values from corners in random order          
+     """
+    def __init__(self, board, character="O", opponentChar="X"):
+        self.board = board
+        self.character = character
+        self.opponentChar = opponentChar 
+        self.corners = {
+                "tl":(0,0,),
+                "tr":(0,2,),
+                "bl":(2,0,),
+                "br":(2,2,),
+                }
+        self.sides = {
+                "tm":(0,1,),
+                "ml":(1,0,),
+                "mr":(1,2,),
+                "bm":(2,1,),
+            }
+        self.randomSides = self.sides.values() 
+        self.randomCorners = self.corners.values()
+
+        #helps make behavior less predictable
+        random.shuffle(self.randomSides)
+        random.shuffle(self.randomCorners)
+
+    def reset(self, board, character="O", opponentChar="X"):
+        """resets the AI allowing it to play multiple games
+
+            Args:
+              board (Board): The board currently in play
+              character (str): The character the AI places on the board
+              opponentChar (str): The opponent's TTT character
+              spotsTaken (int): keeps track of the number of moves made.
+
+            Attributes:
+              board (Board): The board currently in play
+              character (str): The character the AI places on the board
+              opponentChar (str): The opponent's TTT character        
+         """
+        self.board = board
+        self.character = character
+        self.opponentChar = opponentChar
+        random.shuffle(self.randomSides)
+        random.shuffle(self.randomCorners)
+        
+
+    def findBestSpot(self):
+        """Finds the best spot for the next move
+
+            Notes:
+              This is the meat of the AI.  This large function
+              follows the unbeatable TTT strategy on wikipedia
+              to find the best available spot. it uses 2
+              sub-methods in order to reduce a plethora of
+              repeated code.
+
+            Returns:
+              coords (tuple)     
+         """
+        #These are just some shorter names to clean the code
+        board = self.board
+        stage = board.stage
+        default = board.default
+        char = self.character
+        def couldEnd(who):            
+            """Check if the game could end in the next move
+
+                Notes:
+                  Sub-Method of findBestSpot
+                  by making the character in question a parameter,
+                  this method works equally well for finding winning
+                  combinations for the AI and the opponent.
+
+                Arguments:
+                  who (str) character that might achieve the win
+
+                Returns:
+                  coords (tuple) coordinates of winning spot or None     
+             """
+            for i in range(3):
+                #Check for Horizontal win
+                if (stage[i][0] == stage[i][1] == who) and (stage[i][2] == default):
+                    return i,2
+                if (stage[i][1] == stage[i][2] == who) and (stage[i][0] == default):
+                    return i,0
+                if (stage[i][2] == stage[i][0] == who) and (stage[i][1] == default):
+                    return i,1
+
+                #Check for vertical win
+                if (stage[0][i] == stage[1][i] == who) and (stage[2][i] == default):
+                    return 2,i
+                if (stage[1][i] == stage[2][i] == who) and (stage[0][i] == default):
+                    return 0,i
+                if (stage[2][i] == stage[0][i] == who) and (stage[2][i] == default):
+                    return 1,i
+
+                #This stuff could be trimmed down with a loop but would yield no extra clarity or speed
+                
+                #check Diagonals \
+                if (stage[0][0] == stage[1][1] == who) and (stage[2][2] == default):
+                    return 2,2
+                if (stage[1][1] == stage[2][2] == who) and (stage[0][0] == default):
+                    return 0,0
+
+                #Don't need to check mid, it'll already be taken by this point
+
+                #check Diagonals /
+                if (stage[0][2] == stage[1][1] == who) and (stage[2][0] == default):
+                    return 2,0
+                if (stage[1][1] == stage[2][0] == who) and (stage[0][2] == default):
+                    return 0,2
+
+
+        #Check for the 2 way win
+        def checkFork(who):        
+            """Check if the game could end in the next move
+
+                Notes:
+                  Sub-Method of findBestSpot
+                  by making the character in question a parameter,
+                  this method works equally well for finding forks
+                  for the AI or the opponent
+
+                Arguments:
+                  who (str) character that might get a fork
+
+                Returns:
+                  coords (tuple) coordinates of potential fork or None     
+             """
+            def checkCorners(takenA,takenB,emptyA,emptyB):        
+                """Checks if 2 corners are taken and a third is empty
+
+                    Notes:
+                      Sub-Method of checkFork
+                      This method exists entirely to reduce
+                      about 30 lines of repetitous code.
+
+                    Arguments:
+                      takenA (tuple) coordinates of possibly filled corner
+                      takenB (tuple) coordinates of possibly filled corner
+                      emptyA (tuple) coordinates of possibly empty corner
+                      emptyB (tuple) coordinates of possibly empty corner
+
+                    Returns:
+                      coords (tuple) coordinates of good fork corner or None     
+                 """
+            #Check if takenA and takenB are taken
+                if board.spot(*takenA) == board.spot(*takenB) == who:
+                    #emptyA is empty
+                    if board.spotIsEmpty(*emptyA):
+                        return emptyA
+                    #emptyB is empty
+                    if board.spotIsEmpty(*emptyB):
+                        return emptyB
+            
+            #Check top left and top right
+            fork = checkCorners(self.corners["tl"],self.corners["tr"],self.corners["br"],self.corners["bl"])
+            if fork: return fork,self.corners["tl"],self.corners["tr"]
+            #check top right and bottom right
+            fork = checkCorners(self.corners["tr"],self.corners["br"],self.corners["bl"],self.corners["tl"])
+            if fork: return fork,self.corners["tr"],self.corners["br"]
+            #check bottom right and bottom left
+            fork = checkCorners(self.corners["br"],self.corners["bl"],self.corners["tl"],self.corners["tr"])
+            if fork: return fork,self.corners["br"],self.corners["bl"]
+            #check bottom left and top left
+            fork = checkCorners(self.corners["bl"],self.corners["tl"],self.corners["tr"],self.corners["br"])
+            if fork: return fork,self.corners["bl"],self.corners["tl"]
+            #check diagonals \
+            fork = checkCorners(self.corners["tl"],self.corners["br"],self.corners["tr"],self.corners["bl"])
+            if fork: return fork,self.corners["tl"],self.corners["br"]
+            #check diagonals /
+            fork = checkCorners(self.corners["tr"],self.corners["bl"],self.corners["tl"],self.corners["br"])
+            if fork: return fork,self.corners["tr"],self.corners["bl"]
+
+        #Game can't end before 3 moves
+        if len(board) >= 3:
+            #Checks if AI can win
+            willWin = couldEnd(char)
+            if willWin:
+                return willWin
+
+            #Checks if AI can lose next turn
+            couldLose = couldEnd(self.opponentChar)
+            if couldLose:
+                return couldLose
+
+            #checks if AI can fork
+            iCanFork = checkFork(char)
+            if iCanFork:
+                return iCanFork[0]
+
+            #checks if player can fork
+            theyCanFork = checkFork(self.opponentChar)
+            if theyCanFork:
+                row,col = 0,1
+                cornerA, cornerB = theyCanFork[1:]
+                #Does the opponent have two corners on the same row?
+                if cornerA[row] == cornerB[row]: 
+                    if board.spotIsEmpty(cornerA[row],1): #is it already blocked?
+                        return cornerA[row],1 #if not, block it!
+                #Does the opponent have two corners on the same column?
+                elif cornerA[col] == cornerB[col]:
+                    if board.spotIsEmpty(1,cornerA[col]): #is it already blocked?
+                        return 1,cornerA[col] #if not, block it!
+                else: #if the opponent has two diagonal corners
+                    for side in self.randomSides:
+                        if board.spotIsEmpty(*side):
+                            return side#take a random available side
+
+        #grab center
+        if board.spotIsEmpty(1,1):
+            return 1,1
+
+        #grab any corner
+        for corner in self.randomCorners:
+            if board.spotIsEmpty(*corner):
+                return corner
+
+        #grab any side
+        for side in self.randomSides:
+            if board.spotIsEmpty(*side):
+                return side
+
+    def takeTurn(self):
+        """Finds the best spot for the next move and makes the move"""
+        self.board.makeMove(*(list(self.findBestSpot()) + [self.character]))
---
+++ Board.py
@@ -0,0 +1,121 @@
+"""
+Adam Traub
+Tic-Tac-Toe Board
+4/25/2015
+"""
+
+class Board:
+    """Represents a Tic-Tac-Toe board with some handy functions
+
+        Args:
+          default (str): The default character left at blank spots
+
+        Attributes:
+          default (str): The default character left at blank spots.
+          stage (list): A 2-dimensional list representing the board.
+          spotsTaken (int): keeps track of the number of moves made.
+     """
+
+    def __init__(self, default=""):
+        self.default = default
+        self.stage = [[default for i in range(3)] for x in range(3)]
+        self.spotsTaken = 0
+
+    def makeMove(self,x,y,value):        
+        """Makes a Move at the given coords.
+
+            Args:
+              x (int): The value corresponding to x on a cartesian plane.
+              y (int): The value corresponding to y on a cartesian plane.
+              value (str): The value to place onto the board.
+
+            Raises:
+              IndexError: If index out of bounds or spot is taken.
+         """
+        if self.spotIsEmpty(x,y):
+            self.stage[x][y] = value
+            self.spotsTaken += 1
+        else:
+            raise IndexError("That spot is taken")
+
+    def spotIsEmpty(self,x,y):        
+        """Check if a given location is empty.
+
+            Args:
+              x (int): The value corresponding to x on a cartesian plane.
+              y (int): The value corresponding to y on a cartesian plane.
+              
+            Returns:
+              (bool): True if empty False otherwise.
+         """
+        return self.stage[x][y] == self.default
+    
+    def spot(self,x,y):        
+        """Returns element at the given spot
+
+            Args:
+              x (int): The value corresponding to x on a cartesian plane.
+              y (int): The value corresponding to y on a cartesian plane.
+
+            Returns:
+              (probably a string): value located at the given coordinates.
+         """
+        return self.stage[x][y]
+
+    #Returns a list of all open spots on the board
+    def openSpots(self):
+        """Returns a list of all empty coordinates
+
+            Returns:
+              (list of tuples): all available coordinates
+         """
+        return [(i,j) for i in range(3) for j in range(3) if self.spotIsEmpty(i,j)]
+
+    def __len__(self):
+        """Returns the number spots filled (moves made)
+
+            Returns:
+              (int): number of moves made
+         """
+        return self.spotsTaken
+
+    def __str__(self):
+        """Simple String representation of the board
+
+            Returns:
+              (str): representation of the board
+         """
+       return"""{0:^3}|{1:^3}|{2:^3}
+-----------
+{3:^3}|{4:^3}|{5:^3}
+-----------
+{6:^3}|{7:^3}|{8:^3}
+""".format(*(self.stage[0] + self.stage[1] + self.stage[2]))
+
+
+    def gameOver(self):
+       """Checks if the game is over
+
+            Returns:
+              (bool) True if the game is over, False otherwise.
+              (str) Character of winning player if there is one
+                    default character if no winner.
+        """
+        for i in range(3):
+            #Row win
+            if self.stage[i][0] == self.stage[i][1] == self.stage[i][2] != self.default:
+                return True,self.stage[i][0]
+            #column win
+            if self.stage[0][i] == self.stage[1][i] == self.stage[2][i] != self.default:
+                return True,self.stage[0][i]
+        #diagnoal win \
+        if self.stage[0][0] == self.stage[1][1] == self.stage[2][2] != self.default:
+            return True,self.stage[1][1]
+        #diagonal win /
+        if  self.stage[0][2] == self.stage[1][1] == self.stage[2][0] != self.default:
+            return True,self.stage[1][1]
+        #stalemate
+        if self.spotsTaken >= 9:
+            return True,self.default
+        #No winner
+        return False,""
