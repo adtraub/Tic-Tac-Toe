e5d2f1d89a1a55ba77171bd67de79eafd658e024
adtraub <adamtraubsemail@gmail.com>
2015-04-26 23:29:38 -0400

    added better error page for user trying to go to ai directly

--- views/AIHandler.py
+++ views/AIHandler.py
@@ -14,28 +14,31 @@ from ViewHandler import Handler
 class AIHandler(Handler):
     """Checks local dictionary for word"""
     def get(self):
-        stage = json.loads(self.request.get("stage"))
-        char = self.request.get("aiChar")
-        opponentChar = self.request.get("playerChar")
-        board = Board(stage)
+        try:
+            stage = json.loads(self.request.get("stage"))
+            char = self.request.get("aiChar")
+            opponentChar = self.request.get("playerChar")
+            board = Board(stage)
 
-        logging.info("Stage: {}".format(stage))
-        logging.info("Stage type: {}".format(type(stage)))
-        logging.info("AIchar: {}".format(char))
-        logging.info("Player Char: {}".format(opponentChar))
-        logging.info("Player Char: {}".format(opponentChar))
-        logging.info("Turn count {}".format(len(board)))
-        logging.info("Game Board:\n"+str(board))
+            logging.info("Stage: {}".format(stage))
+            logging.info("Stage type: {}".format(type(stage)))
+            logging.info("AIchar: {}".format(char))
+            logging.info("Player Char: {}".format(opponentChar))
+            logging.info("Player Char: {}".format(opponentChar))
+            logging.info("Turn count {}".format(len(board)))
+            logging.info("Game Board:\n"+str(board))
 
-        over,winner = board.gameOver()
-        output = {"over":over,
-                  "winner":winner,
-                  "aiMove":-1,}
-        if not over:
-            aiMove = StaticAI.takeTurn(board, char, opponentChar)
-            board.makeMove(*(list(aiMove)+[char]))
-            logging.info("\n"+str(board))
-            output["over"], output["winner"] = board.gameOver()
-            output["aiMove"] = ((aiMove[0]*3)+aiMove[1])                
+            over,winner = board.gameOver()
+            output = {"over":over,
+                      "winner":winner,
+                      "aiMove":-1,}
+            if not over:
+                aiMove = StaticAI.takeTurn(board, char, opponentChar)
+                board.makeMove(*(list(aiMove)+[char]))
+                logging.info("\n"+str(board))
+                output["over"], output["winner"] = board.gameOver()
+                output["aiMove"] = ((aiMove[0]*3)+aiMove[1])
 
-        self.write(json.dumps(output))
+            self.write(json.dumps(output))
+        except:
+            self.render("bad.html")
